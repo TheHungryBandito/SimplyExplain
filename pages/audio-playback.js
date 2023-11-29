1c92069e726363ff94cdda4f84d0f25bb7728a57
Joshua Crews <joshuacrews16@outlook.com>
2023-11-29 11:41:00 +1000

    updated audio playback
    - reformatted audio-playback.js
    - no longer uses <source> tags, instead modifies the src attribute of the <audio> tag.

--- pages/audio-playback.js
+++ pages/audio-playback.js
@@ -1,7 +1,6 @@
 chrome.runtime.onMessage.addListener(handleMessages);
 
-function handleMessages(message, sender, sendResponse)
-{
+function handleMessages(message, sender, sendResponse) {
     if (message.target != 'offscreen') {
         console.log("Failed");
         return;
@@ -10,18 +9,15 @@ function handleMessages(message, sender, sendResponse)
     const base64String = message.text;
     const byteCharacters = atob(base64String);
     const byteNumbers = new Array(byteCharacters.length);
-    for (let i = 0; i < byteCharacters.length; i++)
-    {
+    for (let i = 0; i < byteCharacters.length; i++) {
         byteNumbers[i] = byteCharacters.charCodeAt(i);
     }
     const byteArray = new Uint8Array(byteNumbers);
-    const audioBlob = new Blob([byteArray], {type: 'audio/mpeg'});
+    const audioBlob = new Blob([byteArray], { type: 'audio/mpeg' });
 
     const audioElement = document.getElementById("audio");
-    const sourceElement = document.getElementById("audio-src");
-    sourceElement.setAttribute("src", URL.createObjectURL(audioBlob));
-    sourceElement.setAttribute("type", 'audio/mpeg');
-    audioElement.appendChild(sourceElement);
+    audioElement.setAttribute("src", URL.createObjectURL(audioBlob));
+    audioElement.setAttribute("type", 'audio/mpeg');
     audioElement.play();
 
     sendResponse(URL.createObjectURL(audioBlob));
