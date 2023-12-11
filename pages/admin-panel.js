04dff236bc07c8e9afbc0c7d95c49c981ed2b971
Joshua Crews <joshuacrews16@outlook.com>
2023-12-11 21:55:58 +1000

    style: reformat, restructure, document admin panel

--- pages/admin-panel.js
+++ pages/admin-panel.js
@@ -8,128 +8,153 @@ window.onload = () => {
   ttsEnabled.addEventListener('click', updateTTSOptions);
   ttsModel.addEventListener('input', updateTTSOptions);
 
-    // Navbar
-    const uninstallBtn = document.getElementById("uninstall-btn");
-    uninstallBtn.addEventListener("click", uninstall);
-    
-    function updateTTSOptions() {
-        if (ttsEnabled.checked)
-        {
-            document.getElementById("tts-model").disabled = false;
-            document.getElementById("tts-voice").disabled = false;
-        } else {
-            document.getElementById("tts-model").disabled = true;
-            document.getElementById("tts-voice").disabled = true;
-        }
+  // Navbar
+  const uninstallBtn = document.getElementById('uninstall-btn');
+  uninstallBtn.addEventListener('click', uninstall);
 
-        if (ttsModel.value == "tts-chrome")
-        {
-            document.getElementById("tts-voice").disabled = true;
-        }
+  /**
+   * Updates the TTS options with selected values.
+   */
+  function updateTTSOptions() {
+    if (ttsEnabled.checked) {
+      document.getElementById('tts-model').disabled = false;
+      document.getElementById('tts-voice').disabled = false;
+    } else {
+      document.getElementById('tts-model').disabled = true;
+      document.getElementById('tts-voice').disabled = true;
     }
 
-    function loadOptions()
-    {
-        chrome.storage.local.get(["OpenAIKey"]).then((storage) => {
-            if (storage.OpenAIKey)
-            {
-                document.getElementById("api-key").value = storage.OpenAIKey;
-            }
-        });
-
-        chrome.storage.sync.get(
-            {
-                "ReadingLevel": "unset",
-                "Persona": "unset",
-                "BotAction": "unset",
-                "WordLimit": "unset",
-                "TTS": "unset",
-                "GPT": "unset",
-                "Voice": "unset",
-                "TTSEnabled": "unset"
-            },
-            function updateOptionFields(botOptions) {
-                // Only set if values are found in storage, so placeholders take place.
-                if (botOptions.ReadingLevel != "unset")
-                {
-                    document.getElementById("reading-level").value = botOptions.ReadingLevel;
-                }
-                if (botOptions.Persona != "unset")
-                {
-                    document.getElementById("persona").value = botOptions.Persona;
-                }
-                if (botOptions.BotAction != "unset")
-                {
-                    document.getElementById("action").value = botOptions.BotAction;
-                }
-                if (botOptions.WordLimit != "unset")
-                {
-                    document.getElementById("word-limit").value = botOptions.WordLimit;
-                }
-                if (botOptions.TTS != "unset")
-                {
-                    document.getElementById("tts-model").value = botOptions.TTS;
-                }
-                if (botOptions.GPT != "unset")
-                {
-                    document.getElementById("gpt-model").value = botOptions.GPT;
-                }
-                if (botOptions.Voice != "unset")
-                {
-                    document.getElementById("tts-voice").value = botOptions.Voice;
-                }
-                if (botOptions.TTSEnabled != "unset")
-                {
-                    ttsEnabled.checked = botOptions.TTSEnabled;
-                    updateTTSOptions();
-                }
-            }
-        );
+    if (ttsModel.value == 'tts-chrome') {
+      document.getElementById('tts-voice').disabled = true;
     }
+  }
 
-    function saveOptions() {
-        const key = document.getElementById("api-key");
-        const ttsVoice = document.getElementById("tts-voice");
-        const gptModel = document.getElementById("gpt-model");
-
-        chrome.storage.local.set({ OpenAIKey: key.value });
-
-        chrome.storage.sync.set({
-            TTS: ttsModel.value.toString().toLowerCase(),
-            GPT: gptModel.value.toString().toLowerCase(),
-            Voice: ttsVoice.value.toString().toLowerCase(),
-            TTSEnabled: ttsEnabled.checked
+  /**
+   * Updates the prompt options with selected values.
+   */
+  function updatePrompt() {
+    const prompt = document.getElementById('prompt');
+    chrome.storage.sync.get(
+        {
+          'Persona': 'teacher',
+          'BotAction': 'explain the concept of the text',
+          'ReadingLevel': 'beginner',
+          'WordLimit': '30',
+        }, function(botOptions) {
+          prompt.textContent = ` You are a/an ${botOptions.Persona} and 
+          ${botOptions.BotAction} the user provides at a/an 
+          ${botOptions.ReadingLevel} level of the topic. 
+          Limit responses to ${botOptions.WordLimit} words.`;
         },
-        function () {
-            loadOptions();
-        });
-    }
-    
-    // Saves extensions settings
-    function savePrompt() {
-        const readingLevel = document.getElementById("reading-level");
-        const persona = document.getElementById("persona");
-        const botAction = document.getElementById("action");
-        const wordLimit = document.getElementById("word-limit");
-        
-        if (Number.isNaN(wordLimit.value))
+    );
+  }
+
+  /**
+   * Loads the user's saved options to display them on the front-end.
+   */
+  function loadOptions() {
+    chrome.storage.local.get(['OpenAIKey']).then((storage) => {
+      if (storage.OpenAIKey) {
+        document.getElementById('api-key').value = storage.OpenAIKey;
+      }
+    });
+
+    chrome.storage.sync.get(
         {
-            console.log('Word limit is not a number, please use a real number');
-            return false;
-        }
+          'ReadingLevel': 'unset',
+          'Persona': 'unset',
+          'BotAction': 'unset',
+          'WordLimit': 'unset',
+          'TTS': 'unset',
+          'GPT': 'unset',
+          'Voice': 'unset',
+          'TTSEnabled': 'unset',
+        }, updateOptionFields,
+    );
+  }
 
-        chrome.storage.sync.set({
-            ReadingLevel: readingLevel.value.toString().toLowerCase(),
-            BotAction: botAction.value.toString().toLowerCase(),
-            Persona: persona.value.toString().toLowerCase(),
-            WordLimit: wordLimit.value,
-        },
-        function () {
-            updatePrompt();
-        });
+  /**
+   * Updates the frontend with the provided options.
+   * @param {*} botOptions loaded options.
+   */
+  function updateOptionFields(botOptions) {
+    if (botOptions.ReadingLevel != 'unset') {
+      document.getElementById('reading-level').value = botOptions.ReadingLevel;
+    }
+    if (botOptions.Persona != 'unset') {
+      document.getElementById('persona').value = botOptions.Persona;
+    }
+    if (botOptions.BotAction != 'unset') {
+      document.getElementById('action').value = botOptions.BotAction;
+    }
+    if (botOptions.WordLimit != 'unset') {
+      document.getElementById('word-limit').value = botOptions.WordLimit;
+    }
+    if (botOptions.TTS != 'unset') {
+      document.getElementById('tts-model').value = botOptions.TTS;
+    }
+    if (botOptions.GPT != 'unset') {
+      document.getElementById('gpt-model').value = botOptions.GPT;
+    }
+    if (botOptions.Voice != 'unset') {
+      document.getElementById('tts-voice').value = botOptions.Voice;
+    }
+    if (botOptions.TTSEnabled != 'unset') {
+      ttsEnabled.checked = botOptions.TTSEnabled;
+      updateTTSOptions();
+    }
+  }
+
+  /**
+   * Saves user's selected options to storage.
+   */
+  function saveOptions() {
+    const key = document.getElementById('api-key');
+    const ttsVoice = document.getElementById('tts-voice');
+    const gptModel = document.getElementById('gpt-model');
+
+    chrome.storage.local.set({OpenAIKey: key.value.toString()});
+
+    chrome.storage.sync.set({
+      TTS: ttsModel.value.toString().toLowerCase(),
+      GPT: gptModel.value.toString().toLowerCase(),
+      Voice: ttsVoice.value.toString().toLowerCase(),
+      TTSEnabled: ttsEnabled.checked,
+    },
+    function() {
+      loadOptions();
+    });
+  }
+
+  /**
+   * Saves the prompt builder options to user's storage.
+   * @return {void}
+   */
+  function savePrompt() {
+    const readingLevel = document.getElementById('reading-level');
+    const persona = document.getElementById('persona');
+    const botAction = document.getElementById('action');
+    const wordLimit = document.getElementById('word-limit');
+
+    if (Number.isNaN(wordLimit.value)) {
+      console.log('Word limit is not a number, please use a real number');
+      return;
     }
 
-    // Uninstalls the chrome extension from the user's browser
+    chrome.storage.sync.set({
+      ReadingLevel: readingLevel.value.toString().toLowerCase(),
+      BotAction: botAction.value.toString().toLowerCase(),
+      Persona: persona.value.toString().toLowerCase(),
+      WordLimit: wordLimit.value,
+    },
+    function() {
+      updatePrompt();
+    });
+  }
+
+  /**
+   * Uninstalls the chrome extension from the user's browser.
+   */
   function uninstall() {
     chrome.management.uninstallSelf({
       showConfirmDialog: true,
@@ -137,23 +162,8 @@ window.onload = () => {
         .catch((err) => {
           console.error('Uninstall failed -', err);
         });
-    }
+  }
 
-    function updatePrompt() {
-        const prompt = document.getElementById("prompt");
-        chrome.storage.sync.get(
-            {
-                "Persona": "teacher",
-                "BotAction": "explain the concept of the text",
-                "ReadingLevel": "beginner",
-                "WordLimit": "30",
-            }, function (botOptions) {
-                prompt.textContent = ` You are a/an ${botOptions.Persona} and ${botOptions.BotAction} the user provides at a/an ${botOptions.ReadingLevel} level of the topic. 
-                Limit responses to ${botOptions.WordLimit} words.`;
-            }
-        );
-    }
-
-    loadOptions();
-    updatePrompt();
+  loadOptions();
+  updatePrompt();
 };
